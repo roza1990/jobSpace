d04d83697b2f4ae53d71091cec4fb765d3c679ce
roza1990 <rozatahmazyan1@gmail.com>
2019-03-27 21:10:44 +0400

    add jobSpace project

---
+++ src/main/java/com/example/jobspace/JobspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobspace/JobspaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jobspace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobspaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
