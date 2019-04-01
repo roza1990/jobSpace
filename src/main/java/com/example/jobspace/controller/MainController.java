cad48ee317682c273485361ea9a7dd1376a1f229
Stella <stella02051994@gmail.com>
2019-04-01 22:40:07 +0400

    xxx

--- src/main/java/com/example/jobspace/controller/MainController.java
+++ src/main/java/com/example/jobspace/controller/MainController.java
@@ -4,6 +4,7 @@ import com.example.jobspace.model.UserType;
 import com.example.jobspace.security.SpringUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -40,7 +41,7 @@ public class MainController {
 
     @GetMapping("/loginSuccess")
     public String loginSuccess(@AuthenticationPrincipal
-                                       SpringUser springUser,ModelMap modelMap) {
+                                       SpringUser springUser, Model modelMap) {
         modelMap.addAttribute("user",springUser);
         if (springUser.getUser().getUserType() == UserType.EMPLOYER) {
             return "redirect:/employer";
