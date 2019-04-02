c3282e290b5eb9a997b8a0e79619916e9f8c7ca1
stellahahmazyan <stella.tahmazyan@scdm.de>
2019-04-02 15:54:46 +0400

    add fb login

--- src/main/java/com/example/jobspace/controller/UserController.java
+++ src/main/java/com/example/jobspace/controller/UserController.java
@@ -7,14 +7,17 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,55 +31,61 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    @Value("${image.upload.dir}")
-    private String imageUploadDir;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private EmailService emailService;
+  @Autowired
+  private Facebook facebook;
+  @Autowired
+  private ConnectionRepository connectionRepository;
 
 
+  @Value("${image.upload.dir}")
+  private String imageUploadDir;
 
-    @GetMapping("/register")
-    public String registerForm (ModelMap map){
-        List<User> all =  userRepository.findAll();
-        map.addAttribute("users", all);
-        return "registration";
-    }
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @PostMapping("/register")
-    public String register(RedirectAttributes redirectAttributes, @ModelAttribute User user , @RequestParam("picture") MultipartFile file) throws IOException {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-        File picture = new File(imageUploadDir + File.separator + fileName);
-        file.transferTo(picture);
-        user.setPicUrl(fileName);
-        userRepository.save(user);
-        redirectAttributes.addFlashAttribute("message", "You are registered successfully!");
-        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+  @Autowired
+  private UserRepository userRepository;
 
-        return "redirect:/";
-    }
+  @Autowired
+  private EmailService emailService;
 
 
-    @GetMapping("/add")
-    public String addUserView(ModelMap map) {
-         userRepository.findAll();
-        return "addUser";
-    }
+  @GetMapping("/register")
+  public String registerForm(ModelMap map) {
+    List<User> all = userRepository.findAll();
+    map.addAttribute("users", all);
+    return "registration";
+  }
+
+  @PostMapping("/register")
+  public String register(RedirectAttributes redirectAttributes, @ModelAttribute User user,
+      @RequestParam("picture") MultipartFile file) throws IOException {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+    File picture = new File(imageUploadDir + File.separator + fileName);
+    file.transferTo(picture);
+    user.setPicUrl(fileName);
+    userRepository.save(user);
+    redirectAttributes.addFlashAttribute("message", "You are registered successfully!");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/add")
+  public String addUserView(ModelMap map) {
+    userRepository.findAll();
+    return "addUser";
+  }
 
 
-    @GetMapping("/getImage")
-    public void getImageAsByteArray(HttpServletResponse response, @RequestParam("picUrl") String picUrl) throws IOException {
-        InputStream in = new FileInputStream(imageUploadDir + File.separator + picUrl);
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        IOUtils.copy(in, response.getOutputStream());
-    }
+  @GetMapping("/getImage")
+  public void getImageAsByteArray(HttpServletResponse response,
+      @RequestParam("picUrl") String picUrl) throws IOException {
+    InputStream in = new FileInputStream(imageUploadDir + File.separator + picUrl);
+    response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+    IOUtils.copy(in, response.getOutputStream());
+  }
 
 
 }
